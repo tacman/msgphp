0f64bbf5fa99bbc2008f3d0e61525ea2795d20ca
Roland Franssen <franssen.roland@gmail.com>
2018-03-17 18:03:53 +0100

    fix AddUserAttributeValueHandler

--- src/User/Command/Handler/AddUserAttributeValueHandler.php
+++ src/User/Command/Handler/AddUserAttributeValueHandler.php
@@ -6,7 +6,7 @@ namespace MsgPhp\User\Command\Handler;
 
 use MsgPhp\Domain\Factory\EntityAwareFactoryInterface;
 use MsgPhp\Domain\Message\{DomainMessageBusInterface, MessageDispatchingTrait};
-use MsgPhp\Eav\Entity\Attribute;
+use MsgPhp\Eav\Entity\{Attribute, AttributeValue};
 use MsgPhp\User\Command\AddUserAttributeValueCommand;
 use MsgPhp\User\Entity\{User, UserAttributeValue};
 use MsgPhp\User\Event\UserAttributeValueAddedEvent;
@@ -30,12 +30,17 @@ final class AddUserAttributeValueHandler
 
     public function __invoke(AddUserAttributeValueCommand $command): void
     {
+        $attributeValueContext = $command->context['attributeValue'] ?? $command->context['attribute_value'] ?? [];
+        if (!isset($attributeValueContext['id'])) {
+            $attributeValueContext['id'] = $this->factory->nextIdentifier(AttributeValue::class);
+        }
+
         $userAttributeValue = $this->factory->create(UserAttributeValue::class, [
             'user' => $this->factory->reference(User::class, $this->factory->identify(User::class, $command->userId)),
             'attributeValue' => [
-                'attribute' => $this->factory->reference(Attribute::class, $command->attributeId),
+                'attribute' => $this->factory->reference(Attribute::class, $this->factory->identify(Attribute::class, $command->attributeId)),
                 'value' => $command->value,
-            ],
+            ] + $attributeValueContext,
         ] + $command->context);
 
         $this->repository->save($userAttributeValue);
