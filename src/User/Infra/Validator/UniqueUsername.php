f66ccaf5652bfb9bb83829b80793104b93945db5
Kirill <sh.kiruh@gmail.com>
2018-08-26 09:33:40 +0300

    Add annotations to user costraints (#180)

--- src/User/Infra/Validator/UniqueUsername.php
+++ src/User/Infra/Validator/UniqueUsername.php
@@ -7,6 +7,9 @@ namespace MsgPhp\User\Infra\Validator;
 use Symfony\Component\Validator\Constraint;
 
 /**
+ * @Annotation
+ * @Target({"PROPERTY", "METHOD", "ANNOTATION"})
+ *
  * @author Roland Franssen <franssen.roland@gmail.com>
  */
 final class UniqueUsername extends Constraint
