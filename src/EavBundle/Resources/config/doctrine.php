d3bddee0ca49cd651c9c1ec3fefb6ef398a28c52
Roland Franssen <franssen.roland@gmail.com>
2018-11-04 14:45:03 +0100

    fix psr4 service loaders

--- src/EavBundle/Resources/config/doctrine.php
+++ src/EavBundle/Resources/config/doctrine.php
@@ -8,15 +8,19 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 use function Symfony\Component\DependencyInjection\Loader\Configurator\ref;
 
 return function (ContainerConfigurator $container): void {
-    $container->services()
+    $services = $container->services()
         ->defaults()
             ->autowire()
             ->autoconfigure()
             ->private()
-
-        ->load(Configuration::PACKAGE_NS.'Infra\\Doctrine\\Repository\\', Configuration::getPackageGlob().'/Infra/Doctrine/Repository/*Repository.php')
-            ->bind(EntityManagerInterface::class, ref('msgphp.doctrine.entity_manager'))
-
-        ->load(Configuration::PACKAGE_NS.'Infra\\Doctrine\\', Configuration::getPackageGlob().'/Infra/Doctrine/*ObjectMappings.php')
     ;
+
+    foreach (Configuration::getPackageDirs() as $dir) {
+        $services
+            ->load(Configuration::PACKAGE_NS.'Infra\\Doctrine\\Repository\\', $dir.'/Infra/Doctrine/Repository/*Repository.php')
+                ->bind(EntityManagerInterface::class, ref('msgphp.doctrine.entity_manager'))
+
+            ->load(Configuration::PACKAGE_NS.'Infra\\Doctrine\\', $dir.'/Infra/Doctrine/*ObjectMappings.php')
+        ;
+    }
 };
